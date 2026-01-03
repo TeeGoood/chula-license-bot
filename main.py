2b4178e543bb70404355708cabd82a592d2ec77a
teegooodgood <teegooodgood@gmail.com>
2026-01-03 20:32:22 +0700

    refactor code

--- main.py
+++ main.py
@@ -8,44 +8,89 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
 
-load_dotenv()
-
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-
-url = "https://licenseportal.it.chula.ac.th"
-email = os.getenv("LOGIN_EMAIL") or ""
-password = os.getenv("LOGIN_PASSWORD") or ""
-options = {
-    "adobe": "5",
-    "foxit": "7",
-    "zoom": "2",
-}
-option = "foxit"
-now = datetime.now()
-start = now
-end = start + timedelta(days=7)
-
 
 def main():
+    load_dotenv()
+    setup_logger()
+
+    url = "https://licenseportal.it.chula.ac.th"
+    email = os.getenv("LOGIN_EMAIL") or ""
+    password = os.getenv("LOGIN_PASSWORD") or ""
+
+    select_options = {
+        "adobe": "5",
+        "foxit": "7",
+        "zoom": "2",
+    }
+
+    now = datetime.now()
+    start = now
+    end = start + timedelta(days=7)
+
+    licenses = get_licenses()
+    last_borrowed = get_last_borrowed()
+
+    with sync_playwright() as playwright:
+        # setup
+        browser = playwright.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+
+        # login
+        page.goto(url)
+        page.locator("#UserName").fill(email)
+        page.locator("#Password").fill(password)
+        page.locator("button[type='submit']").click()
+
+        # borrow licenses
+        for license in licenses:
+            # check borrow period
+            if now - last_borrowed[license] < timedelta(days=6):
+                logging.info(
+                    f"{license} don't reach borrow period, will be borrowed in {6 - (now - last_borrowed[license]).days} days"
+                )
+                continue
+
+            page.goto(f"{url}/Home/Borrow")
+            page.locator("#ProgramLicenseID").select_option(select_options[license])
+            page.locator("#BorrowDateStr").fill(start.strftime("%d/%m/%Y"))
+            page.keyboard.press("Escape")
+            page.locator("#ExpiryDateStr").fill(end.strftime("%d/%m/%Y"))
+            page.keyboard.press("Escape")
+            page.get_by_role("button", name="Save").click()
+
+            last_borrowed[license] = now
+            logging.info(f"Borrow {license} success")
+
+        pickle.dump(last_borrowed, open("last_borrowed.pickle", "wb"))
+
+
+def setup_logger():
+    logging.basicConfig(
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        level=logging.INFO,
+    )
+
+
+def get_licenses():
     if len(sys.argv) < 2:
         print(
             textwrap.dedent("""
-          Usage: python main.py <application> [<application> ...]
+        Usage: python main.py <license> [<license> ...]
 
-          Applications:
-            foxit | zoom | adobe
+        License:
+          foxit | zoom | adobe
 
-          Example:
-            python main.py foxit zoom adobe
-        """)
+        Example:
+          python main.py foxit zoom
+      """)
         )
-        return
+        exit(1)
 
-    programs = sys.argv[1:]
+    return sys.argv[1:]
 
+
+def get_last_borrowed():
     try:
         with open("last_borrowed.pickle", "rb") as f:
             last_borrowed = pickle.load(f)
@@ -56,44 +101,11 @@ def main():
             "foxit": epoch,
             "zoom": epoch,
         }
-
-    try:
-        with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(headless=True)
-            context = browser.new_context()
-            page = context.new_page()
-
-            # login
-            page.goto(url)
-            page.locator("#UserName").fill(email)
-            page.locator("#Password").fill(password)
-            page.locator("button[type='submit']").click()
-
-            # borrow license
-            for program in programs:
-                if now - last_borrowed[program] < timedelta(days=6):
-                    logging.info(
-                        f"{program} don't reach borrow period, {(now - last_borrowed[option]).days} days"
-                    )
-                    continue
-
-                page.goto(f"{url}/Home/Borrow")
-                page.locator("#ProgramLicenseID").select_option(options[program])
-                page.locator("#BorrowDateStr").fill(start.strftime("%d/%m/%Y"))
-                page.keyboard.press("Escape")
-                page.locator("#ExpiryDateStr").fill(end.strftime("%d/%m/%Y"))
-                page.keyboard.press("Escape")
-                page.get_by_role("button", name="Save").click()
-
-                last_borrowed[program] = now
-
-            context.close()
-            browser.close()
-            pickle.dump(last_borrowed, open("last_borrowed.pickle", "wb"))
-            logging.info(f"Borrow {', '.join(programs)} success")
-    except Exception:
-        logging.exception("Borrow failed")
+    return last_borrowed
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        logging.exception("Borrow failed")
