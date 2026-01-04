e63a2b92543bac32bf43d587d9d757d8bd981264
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2026-01-04 20:03:45 +0700

    fix: remove unverify cuz i forgot i used to debug

--- client.py
+++ client.py
@@ -38,7 +38,6 @@ class PortalClient:
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
             }
         )
-        self.session.verify = False
 
     def _get_form_payload(
         self, html: str, form_action: Optional[str] = None
